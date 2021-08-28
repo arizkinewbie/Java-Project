0020bdf7e27145bcb003ad2dac875b90de84c5f3
lenovo <lenovo@192.168.1.152>
2021-08-28 22:13:34 +0700

    Continue

---
+++ src/Continue.java
@@ -0,0 +1,14 @@
+public class Continue {
+
+   public static void main(String args[]){
+	for (int j=0; j<=6; j++)
+	{
+           if (j==4)
+           {
+	      continue;
+	   }
+
+           System.out.println(j+" ");
+	}
+   }
+}
