df440129eeb89ec07fdde9735ca6d667f2ccdf87
lenovo <lenovo@192.168.1.152>
2021-08-29 16:06:14 +0700

    Use of break in a for loop

---
+++ src/Useofbreakinaforloop.java
@@ -0,0 +1,15 @@
+public class Useofbreakinaforloop {
+
+   public static void main(String args[]){
+	int var;
+	for (var =100; var>=10; var --)
+	{
+	    System.out.println("var: "+var);
+	    if (var==99)
+	    {
+	         break;
+	    }
+	 }
+	 System.out.println("Out of for-loop");
+   }
+}
